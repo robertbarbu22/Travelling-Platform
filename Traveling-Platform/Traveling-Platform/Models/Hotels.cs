b3b181a83afc576880fdc436d02ba8382ca00a06
aberbecaru <andreiberbecaru02#gmail.com>
2023-03-22 19:45:35 +0200

    new update

--- Traveling-Platform/Traveling-Platform/Models/Hotels.cs
+++ Traveling-Platform/Traveling-Platform/Models/Hotels.cs
@@ -1,6 +1,15 @@
-﻿namespace Traveling_Platform.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Traveling_Platform.Models
 {
     public class Hotels
     {
+
+        [Key]
+        public int id_hotel { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
     }
 }
