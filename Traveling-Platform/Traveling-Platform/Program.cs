9433ac23c723198742fe04af1c8e97c34dbac8f6
Radu Constantin Ionescu <raduionescu2002@gmail.com>
2023-03-27 10:41:31 +0300

    db done

--- Traveling-Platform/Traveling-Platform/Program.cs
+++ Traveling-Platform/Traveling-Platform/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
+// da
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
