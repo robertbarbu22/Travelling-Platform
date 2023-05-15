44b6ebfa8688c422182f7f87d1662653425c6b0e
Rares Stefanoiou <rares.stefanoiu@gmail.com>
2023-05-15 11:55:26 +0300

    update cool

---
+++ Traveling-Platform/Traveling-Platform/Models/ReviewViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Traveling_Platform.Models
+{
+    public class ReviewViewModel
+    {
+        public int Id { get; set; }
+
+        
+        public string Text { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string ClientName { get; set; }
+
+
+        public string HotelName { get; set; }
+
+    }
+}
